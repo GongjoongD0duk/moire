0e9074b55f3e325f89f91724677e06f6a279dc0a
GongjoongDoduk <noreply@github.com>
2024-10-18 15:21:58 +0900

    Add `EntityDecoratorTest` class

---
+++ src/test/kotlin/org/gjdd/moire/decorators/entities/EntityDecoratorTest.kt
@@ -0,0 +1,32 @@
+package org.gjdd.moire.decorators.entities
+
+import net.fabricmc.api.ModInitializer
+import net.minecraft.entity.mob.CreeperEntity
+import net.minecraft.item.Items
+import org.gjdd.moire.elements.*
+import org.gjdd.moire.matrices.rotateLocalYDegrees
+
+class EntityDecoratorTest : ModInitializer {
+    private val testEntityDecorator = entityDecorator<CreeperEntity> {
+        elementHolder {
+            itemDisplayElement {
+                onTick {
+                    transform {
+                        rotateLocalYDegrees(11.25f)
+                    }
+
+                    startInterpolation(1)
+                }
+
+                item = Items.TNT.defaultStack
+                ignorePositionUpdates()
+            }
+
+            addVirtualPassengerTo(entity)
+        }
+    }
+
+    override fun onInitialize() {
+        EntityDecorators.register(testEntityDecorator)
+    }
+}
